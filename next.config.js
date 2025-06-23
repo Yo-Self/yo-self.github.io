f37f92e52956a059ac4d8b39d634d3fc58ea4f10
Nathan Freire <nmf2@cin.ufpe.br>
2025-06-23 09:36:26 -0300

    Remove um dos next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // basePath: '/NOME-DO-REPO', // Descomente e ajuste se o projeto não estiver na raiz do domínio
-  // assetPrefix: '/NOME-DO-REPO/',
-};
-
-module.exports = nextConfig; 
