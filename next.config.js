b8c0441ae2c5876a440507909726cacd24abe336
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2025-08-11 17:12:04 -0300

    Remove assetPrefix from Next.js configuration to simplify setup for non-production environments.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   images: { unoptimized: true },
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
 };
 
 export default nextConfig;
