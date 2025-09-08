b1eebb20223a99ec8620103aa581dfbe4605ca9d
Jessé Monteiro <jessemonteiroferreira@gmail.com>
2025-09-08 09:57:07 -0300

    Updates

---
+++ _next/static/chunks/app/restaurant/[slug]/page-91382bd7314e2bb8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[767],{8454:function(n,e,u){Promise.resolve().then(u.bind(u,8215))}},function(n){n.O(0,[475,189,932,678,215,971,117,744],function(){return n(n.s=8454)}),_N_E=n.O()}]);
